610b84f19c8e4373cc0a6e00217366d12112b7af
MatusevichVaclav <rialon1@mail.ru>
2015-10-26 20:28:38 +0300

    test

--- Demotivators/WebApplication2/Models/Item.cs
+++ Demotivators/WebApplication2/Models/Item.cs
@@ -11,7 +11,7 @@ namespace WebApplication2.Models
 {
     using System;
     using System.Collections.Generic;
-    
+    //Test
     public partial class Item
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
